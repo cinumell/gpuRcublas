755ea9bb4d560321998d018da57a049971f2e374
Chaitanya Inumella <cinumell@asu.edu>
2022-03-22 11:16:44 -0700

    Adjusted the changes in wrappers.R

--- R/wrappers.R
+++ R/wrappers.R
@@ -50,13 +50,13 @@ cusolver_gesvd <- function(A){
 
   cat("sizeof(L) = (", L, ")\n")
   
-  U  <- matrix(0,nrow=1,ncol=L*M);  # m-by-m unitary matrix, left singular vectors
-  S  <- matrix(0,nrow=1,ncol=N);    # numerical singular value 
-  VT <- matrix(0,nrow=1,ncol=L*N)   # n-by-n unitary matrix, right singular vectors 
+  U  <- matrix(0,nrow=M,ncol=M);  # m-by-m unitary matrix, left singular vectors
+  S  <- matrix(0,nrow=N,ncol=1);    # numerical singular value 
+  VT <- matrix(0,nrow=N,ncol=N)   # n-by-n unitary matrix, right singular vectors 
 
-  U  <- cudaMatrix(U, nrow = 1, ncol = L*M, type = type)
-  S  <- cudaMatrix(S, nrow = 1, ncol = N, type = type)
-  VT <- cudaMatrix(VT, nrow = 1, ncol = L*N, type = type)
+  U  <- cudaMatrix(U, nrow = M, ncol = M, type = type)
+  S  <- cudaMatrix(S, nrow = N, ncol = 1, type = type)
+  VT <- cudaMatrix(VT, nrow = N, ncol = N, type = type)
   
   print('output initialized')
 
