84d8d08c2ffbf50a7f8f9292dc70934ef4ba2360
Chaitanya Inumella <cinumell@asu.edu>
2022-04-17 19:02:30 -0700

    Revoked the changes introduced for cub header files in configure and configure.ac

--- install.R
+++ install.R
@@ -1,13 +1,12 @@
 #!/usr/bin/env R
 # 4.1
 
-mypath="/data/sse/scientific-software/cinumell/rmagma_test/gpuRcublas_new_benchmark/lib"
-
+mypath=file.path(getwd(), "lib")
 #ENV BACKEND=CUDA
 library('devtools')
-withr::with_libpaths(new=mypath,devtools::install_github('wrathematics/thrust'))
+withr::with_libpaths(new=mypath,devtools::install_github('cinumell/thrust'))
 withr::with_libpaths(new=mypath,devtools::install_github('RcppCore/Rcpp'))
-withr::with_libpaths(new=mypath,devtools::install_github('cinumell/gpuRcuda',ref="test_fix"))
+withr::with_libpaths(new=mypath,devtools::install_github('cinumell/gpuRcuda'))
 .libPaths( c(.libPaths(), mypath))
 library('gpuRcuda')
-withr::with_libpaths(new=mypath,devtools::install_github('cinumell/gpuRcublas_new', force=TRUE))
+withr::with_libpaths(new=mypath,devtools::install_github('cinumell/gpuRcublas_new'))
